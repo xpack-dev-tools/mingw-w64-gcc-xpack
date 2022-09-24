ca28612bf8e6aa3038ef4df376f1dbec75a80cde
Liviu Ionescu <ilg@livius.net>
2022-09-24 17:16:34 +0300

    common-apps: binutils --enable-build-warnings=no

--- scripts/common-apps-functions-source.sh
+++ scripts/common-apps-functions-source.sh
@@ -126,6 +126,7 @@ function build_mingw2_binutils()
           config_options+=("--enable-static")
           config_options+=("--enable-shared")
 
+          config_options+=("--enable-build-warnings=no")
           config_options+=("--enable-gold")
           config_options+=("--enable-libssp")
           config_options+=("--enable-lto") # Arch
