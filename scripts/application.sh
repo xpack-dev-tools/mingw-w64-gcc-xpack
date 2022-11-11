8da4f322b761c509e85cf8956e870f2b82c54ff9
Liviu Ionescu <ilg@livius.net>
2022-11-11 19:00:38 +0200

    move dependencies to helper

--- scripts/application.sh
+++ scripts/application.sh
@@ -22,8 +22,8 @@ XBB_APPLICATION_DISTRO_TOP_FOLDER=${XBB_APPLICATION_DISTRO_TOP_FOLDER:-"xPacks"}
 
 XBB_APPLICATION_DESCRIPTION="${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME}"
 
-declare -a XBB_APPLICATION_DEPENDENCIES=( binutils-mingw mingw gcc-mingw )
-declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( libiconv zlib gmp mpfr mpc isl xz zstd )
+declare -a XBB_APPLICATION_DEPENDENCIES=( )
+declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( binutils-mingw mingw gcc-mingw libiconv zlib gmp mpfr mpc isl xz zstd )
 
 # -----------------------------------------------------------------------------
 
