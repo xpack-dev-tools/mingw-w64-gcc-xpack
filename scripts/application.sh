fd4f906ec64490a07c06a3c07694af90a20fc06f
Liviu Ionescu <ilg@livius.net>
2022-11-18 19:15:59 +0200

    application.sh: update deps to binutils

--- scripts/application.sh
+++ scripts/application.sh
@@ -23,7 +23,7 @@ XBB_APPLICATION_DISTRO_TOP_FOLDER=${XBB_APPLICATION_DISTRO_TOP_FOLDER:-"xPacks"}
 XBB_APPLICATION_DESCRIPTION="${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME}"
 
 declare -a XBB_APPLICATION_DEPENDENCIES=( )
-declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( binutils-mingw mingw gcc-mingw libiconv zlib gmp mpfr mpc isl xz zstd )
+declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( binutils mingw gcc-mingw libiconv zlib gmp mpfr mpc isl xz zstd )
 
 # -----------------------------------------------------------------------------
 
