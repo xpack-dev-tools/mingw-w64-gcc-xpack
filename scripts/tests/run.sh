c4f2ab00459a18a4a6b712359b111aecf3cc6190
Liviu Ionescu <ilg@livius.net>
2022-12-15 22:42:19 +0200

    tests/run.sh: update

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -13,14 +13,23 @@ function tests_run_all()
 {
   local test_bin_path="$1"
 
+  xbb_reset_env
+
+  if [ "${XBB_REQUESTED_HOST_PLATFORM}" == "win32" ]
+  then
+    xbb_set_target "mingw-w64-cross"
+  else
+    xbb_set_target "mingw-w64-native"
+  fi
+
   XBB_MINGW_TRIPLETS=( "x86_64-w64-mingw32" "i686-w64-mingw32" )
   for triplet in "${XBB_MINGW_TRIPLETS[@]}"
   do
 
     # Call the functions defined in the build code.
-    test_mingw2_binutils "${test_bin_path}" "${triplet}"
+    test_binutils "${test_bin_path}" "${triplet}-"
 
-    test_mingw2_gcc "${test_bin_path}" "${triplet}"
+    test_mingw_gcc "${test_bin_path}" "${triplet}"
 
   done
 }
