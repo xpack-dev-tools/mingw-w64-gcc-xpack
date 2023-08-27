88a4ea754ec4462ffd93ab1538846484adaef34b
Liviu Ionescu <ilg@livius.net>
2023-08-27 20:47:40 +0300

    application.sh: add ncurses & texinfo deps

--- scripts/application.sh
+++ scripts/application.sh
@@ -23,7 +23,7 @@ XBB_APPLICATION_DISTRO_TOP_FOLDER=${XBB_APPLICATION_DISTRO_TOP_FOLDER:-"xPacks"}
 XBB_APPLICATION_DESCRIPTION="${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME}"
 
 declare -a XBB_APPLICATION_DEPENDENCIES=( )
-declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( binutils mingw gcc-mingw libiconv zlib gmp mpfr mpc isl xz zstd compiler-tests )
+declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( ncurses texinfo binutils mingw gcc-mingw libiconv zlib gmp mpfr mpc isl xz zstd compiler-tests )
 
 # List the DLLs in the results section, at the end.
 XBB_APPLICATION_SHOW_DLLS="y"
