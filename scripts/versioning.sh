f0e980363401a29ab850999aac3147bd15e1dfb1
Liviu Ionescu <ilg@livius.net>
2024-04-22 19:21:02 +0300

    versioning.sh: remove XBB_NCURSES_DISABLE_WIDEC

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -25,11 +25,7 @@ function gcc_mingw_build_common()
 
   libiconv_build "${XBB_LIBICONV_VERSION}"
 
-  (
-    XBB_NCURSES_DISABLE_WIDEC="y"
-
-    ncurses_build "${XBB_NCURSES_VERSION}"
-  )
+  ncurses_build "${XBB_NCURSES_VERSION}"
 
   # new makeinfo needed by binutils 2.41 and up
   # checking for suffix of object files...   MAKEINFO doc/bfd.info
