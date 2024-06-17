c88617062a9203b4eafe4f45472d47c455ac6b37
Liviu Ionescu <ilg@livius.net>
2024-06-18 00:15:42 +0300

    scripts/tests/update.sh: simplify

--- scripts/tests/update.sh
+++ scripts/tests/update.sh
@@ -11,32 +11,13 @@
 
 function tests_update_system()
 {
+  echo
+  echo "[${FUNCNAME[0]} $@]"
+
   local image_name="$1"
 
-  # Make sure that the minimum prerequisites are met.
-  if [[ ${image_name} == github-actions-ubuntu* ]]
-  then
-    : # sudo apt-get -qq install -y XXX
-  elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
-  then
-    run_verbose apt-get -qq install --yes libc6-dev libstdc++6 # TODO: get rid of them
-  elif [[ ${image_name} == *centos* ]] || [[ ${image_name} == *redhat* ]] || [[ ${image_name} == *fedora* ]]
-  then
-    run_verbose yum install --assumeyes --quiet glibc-devel libstdc++-devel # TODO: get rid of them
-  elif [[ ${image_name} == *suse* ]]
-  then
-    run_verbose zypper --quiet --no-gpg-checks install --no-confirm glibc-devel libstdc++6 # TODO: get rid of them
-  elif [[ ${image_name} == *manjaro* ]]
-  then
-    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc-libs # TODO: get rid of them
-  elif [[ ${image_name} == *archlinux* ]]
-  then
-    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc-libs
-  fi
-
-  echo
-  echo "The system C/C++ libraries..."
-  find /usr/lib* /lib -name 'libc.*' -o -name 'libstdc++.*' -o -name 'libgcc_s.*'
+  # Placeholder
 }
+
 
 # -----------------------------------------------------------------------------
