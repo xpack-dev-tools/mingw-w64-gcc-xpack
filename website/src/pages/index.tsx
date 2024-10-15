ce23caea419c9a2f92e13245e296022335463861
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:33:58 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to mingw-w64-gcc`}
+      title="Welcome!"
       description="A binary package with the mingw-w64-gcc executables">
       <HomepageHeader />
       <main>
