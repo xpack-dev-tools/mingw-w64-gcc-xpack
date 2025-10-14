662d4eedb2a30044e69d88208780bb7c1a670f25
Liviu Ionescu <ilg@livius.net>
2025-10-14 20:45:04 +0300

    texinfo rationale

--- build-assets/scripts/versioning.sh
+++ build-assets/scripts/versioning.sh
@@ -27,7 +27,9 @@ function gcc_mingw_build_common()
 
   ncurses_build "${XBB_NCURSES_VERSION}" --hack-links
 
-  # new makeinfo needed by binutils 2.41 and up
+  # texinfo is mandatory on macOS; it is kept on Linux for consistency;
+  # it is required by binutils 2.41 and up.
+
   # checking for suffix of object files...   MAKEINFO doc/bfd.info
   # /Users/ilg/Work/xpack-dev-tools-build/riscv-none-elf-gcc-13.2.0-1/darwin-x64/sources/binutils-2.41/bfd/doc/bfd.texi:245: Node `Sections' requires a sectioning command (e.g., @unnumberedsubsec).
   # Note: binutils_build needs xbb_activate_installed_bin.
